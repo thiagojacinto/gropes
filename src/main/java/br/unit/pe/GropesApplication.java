1132565386d3f263d25bffe3a1de99cf0464c5fc
Andre Fillipe <andre.fillipe20@gmail.com>
2021-05-13 19:58:41 -0300

    incluso suporte a cors mapping

--- src/main/java/br/unit/pe/GropesApplication.java
+++ src/main/java/br/unit/pe/GropesApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GropesApplication {
@@ -67,5 +69,17 @@ public class GropesApplication {
 		Calculos.relevancia(10,listaEmps,listaEmpsItem,tecs);*/
 		SpringApplication.run(GropesApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");;
+			}
+		};
+	}
+
 
 }
